a0af8696913a13db48664caf1ea89f29b4d8a22d
odddyyy <56987696+odddyyy@users.noreply.github.com>
2020-04-17 12:06:10 +0700

    adding date occupied in Rooms table

---
+++ server/migrations/20200417050425-add-date_occupied-in-room-table.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+     return queryInterface.addColumn('Rooms', 'date_occupied', Sequelize.DATE);
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
